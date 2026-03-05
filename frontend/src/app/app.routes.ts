4bb3a8132835a209687450446d375e4c12137960
Salah <salaheddine.dahbani-rajih@iesruizgijon.com>
2026-03-05 19:28:50 +0100

    terminado pagina de proposito

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AlimentosApiComponent } from './components/alimentos-api/alimentos-api.component';
 import { DiarioComidaComponent } from './components/diario-comida/diario-comida.component';
+import { PropositoComponent } from './components/proposito/proposito.component';
 
 export const routes: Routes = [
   { path: 'buscar-alimentos', component: AlimentosApiComponent },
   { path: 'mi-diario', component: DiarioComidaComponent },
+  { path: 'proposito', component: PropositoComponent },
   { path: '', redirectTo: '/buscar-alimentos', pathMatch: 'full' }
 ];
---
+++ frontend/src/app/components/proposito/proposito.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proposito',
+  imports: [],
+  templateUrl: './proposito.component.html',
+  styleUrl: './proposito.component.css'
+})
+export class PropositoComponent {
+
+}
